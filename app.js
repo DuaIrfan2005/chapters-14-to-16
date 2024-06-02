4859a4b767db1f703483386cb9b514a71943b16c
DuaIrfan2005 <155565372+DuaIrfan2005@users.noreply.github.com>
2024-06-02 17:37:33 +0500

    index.html

---
+++ app.js
@@ -0,0 +1,21 @@
+// chapetr 14 to 16
+// qusrtion 1
+
+let futureStudentNames = [];
+console.log(utureStudentNames);
+// qustion 2
+
+let colors = ["red", "blue", "green", "yellow"];
+console.log(colors);
+// qustion 3
+
+let num = ["0", "1", "2", "3"];
+console.log(num);
+// qustion 4
+let booleanArray = [true, false, true, false];
+console.log(booleanArray);
+
+// qustion 5
+let mixedArray = ["apple", 42, true, {name: "John"}, null];
+console.log(mixedArray);
+
